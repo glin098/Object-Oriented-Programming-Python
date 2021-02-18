ac9d7bd2067e458928219d381be51f9d6e7b1a2b
Grace Lin <56848949+glin098@users.noreply.github.com>
2021-02-17 21:01:24 -0600

    Add files via upload

---
+++ name.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+words = name.split()
+first = words[0]
+middle = words[1][0]
+last = words[2]
+print(first, middle + '.', last)
---
+++ wordcount.py
@@ -0,0 +1,4 @@
+raw = input('Enter a sentence: ')
+words = raw.split()
+wordCount = len(words)
+print('You entered', wordCount, 'words.')
