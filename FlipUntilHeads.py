96f1a644a2c0ee7dedb4c2adb3747dee573419cf
Grace Lin <56848949+glin098@users.noreply.github.com>
2021-03-13 23:20:11 -0600

    Add files via upload

---
+++ FlipUntilHeads.py
@@ -0,0 +1,23 @@
+#Consider a single experiment of flipping a fair coin until an outcome
+#of “heads” and counting the number of flips that occur.
+#Write a for loop that repeats 10,000 trials of this experiment
+
+The average number of flips used per trial
+The maximum number of flips that occurred in a single trial
+
+from random import randrange
+
+clist = []
+count = 0
+
+for x in range(1,10000+1):
+    flip = randrange(2)
+    if (flip == 0): #tails
+        count+=1
+    elif (flip == 1): #heads
+        if count != 0:
+            clist.append(count)
+            count = 0
+            tails = False
+print('Average number of flips: ', sum(clist)/len(clist))
+print('Maximum number of flips: ', max(clist))
